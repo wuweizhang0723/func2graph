e345b8857de2a9a481c0263c03d9753cc081544e
wuweizhang0723 <zwuwei0723@outlook.com>
2023-11-12 13:12:55 -0800

    update

--- scripts/Attention_Autoencoder.sh
+++ scripts/Attention_Autoencoder.sh
@@ -13,3 +13,6 @@ python train_model.py --out_folder='../output/11_5_cell_type_W_AA/' --model_type
 
 
 python train_model.py --out_folder='../output/11_8_strength/' --model_type=Attention_Autoencoder --data_type=wuwei --weight_type=cell_type --neuron_num=200 --task_type=prediction --window_size=200 --predict_window_size=1 --hidden_size_1=32 --h_layers_1=0 --heads=1 --attention_layers=1 --hidden_size_2=64 --h_layers_2=0 --learning_rate=5e-5 --pos_enc_type=lookup_table --model_random_seed=42 --data_random_seed=42 --dim_key=200 --attention_activation=none --scheduler=cycle --weight_decay=0
+
+
+python train_model.py --out_folder='../output/11_11_key_dim/' --model_type=Attention_Autoencoder --data_type=wuwei --weight_type=cell_type --neuron_num=200 --task_type=prediction --window_size=200 --predict_window_size=1 --hidden_size_1=32 --h_layers_1=0 --heads=1 --attention_layers=1 --hidden_size_2=64 --h_layers_2=0 --learning_rate=5e-5 --pos_enc_type=lookup_table --model_random_seed=42 --data_random_seed=42 --dim_key=350 --attention_activation=none --scheduler=cycle --weight_decay=0

8926271daa911cd73836fb0e6c1a78dff8478f1d
wuweizhang0723 <zwuwei0723@outlook.com>
2024-05-13 15:02:04 -0700

    add statistical baselines and GLM_sim

--- func2graph/baselines.py
+++ func2graph/baselines.py
@@ -1,11 +1,13 @@
 from math import ceil
 import numpy as np
 from scipy import stats
+from sklearn.feature_selection import mutual_info_regression
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import pytorch_lightning as pl
+from pyinform import transferentropy, mutualinfo
 from func2graph.layers import (
     Residual,
     Residual_For_Attention,
@@ -19,7 +21,6 @@ from func2graph.layers import (
 # Baseline_1:
 # - Pearson correlation
 # - Cross correlation
-# - Granger Causality      # TODO
 ###################################################################################################
 
 
@@ -49,6 +50,83 @@ def get_activity_cross_correlation_matrix(activity, tau=1):
 
 
 
+
+###################################################################################################
+# Mutual Information
+###################################################################################################
+
+def get_activity_mutual_information(activity, tau=1, bins=10):
+    neuron_num = activity.shape[0]
+    mutual_information = np.zeros((neuron_num, neuron_num))
+
+    bin_edges = np.linspace(np.min(activity), np.max(activity), bins + 1)
+
+    for i in range(neuron_num):
+        for j in range(i, neuron_num):
+            if tau == 0:
+                presynaptic = activity[j]
+                postsynaptic = activity[i]
+            else:
+                presynaptic = activity[j, :-tau]
+                postsynaptic = activity[i, tau:]
+
+            digitized_presynaptic = np.digitize(presynaptic, bins=bin_edges)
+            digitized_postsynaptic = np.digitize(postsynaptic, bins=bin_edges)
+
+            # Calculate mutual information
+            score = mutualinfo.mutual_info(digitized_presynaptic, digitized_postsynaptic)
+
+            mutual_information[i, j] = score
+            mutual_information[j, i] = score
+
+    return mutual_information
+
+
+
+###################################################################################################
+# Transfer Entropy
+###################################################################################################
+
+def get_activity_transfer_entropy(activity, history=1, bins=10):
+    """
+    Compute the NxN connectivity matrix using transfer entropy.
+
+    Parameters:
+    activity (numpy.ndarray): NxT matrix of neural activity.
+    history (int): The number of past time steps to consider for each neuron.
+    future (int): The number of future time steps to predict.
+
+    Returns:
+    numpy.ndarray: NxN matrix of transfer entropy values.
+    """
+    N, T = activity.shape
+    connectivity_matrix = np.zeros((N, N))
+
+    bin_edges = np.linspace(np.min(activity), np.max(activity), bins + 1)
+
+    # Loop over all pairs of neurons
+    for source in range(N):
+        for target in range(N):
+            # if source != target:  # Avoid self-comparison, if desired
+            
+            # Extract the time series for the source and target
+            src_series = activity[source, :]
+            tgt_series = activity[target, :]
+
+            digitized_presynaptic = np.digitize(src_series, bins=bin_edges)
+            digitized_postsynaptic = np.digitize(tgt_series, bins=bin_edges)
+
+            # Calculate transfer entropy from source to target
+            te = transferentropy.transfer_entropy(digitized_presynaptic, digitized_postsynaptic, k=history)
+
+            # Store the transfer entropy in the matrix
+            connectivity_matrix[target, source] = te
+
+    return connectivity_matrix
+
+
+
+
 ###################################################################################################
 # Baseline_2:
 # - Base_2 is the base for Baseline_2
@@ -74,7 +152,7 @@ class Base_2(pl.LightningModule):
                     optimizer,
                     patience=6,
                 ),
-                "monitor": str(self.hparams.loss_function) + " val_loss",
+                "monitor": "val_loss",
             }
         elif self.hparams.scheduler == "cycle":
             lr_scheduler = {
@@ -95,47 +173,29 @@ class Base_2(pl.LightningModule):
         x, y = batch
         y_hat = self(x)
 
-        if self.hparams.loss_function == "mse":
-            loss = F.mse_loss(y_hat, y, reduction="mean")
-        elif self.hparams.loss_function == "poisson":
-            if self.hparams.log_input:
-                loss = F.poisson_nll_loss(y_hat, x, log_input=True, reduction="mean")
-            else:
-                loss = F.poisson_nll_loss(y_hat, x, log_input=False, reduction="mean")
+        loss = F.mse_loss(y_hat, y, reduction="mean")
 
-        self.log(str(self.hparams.loss_function) + " train_loss", loss)
+        self.log("train_loss", loss)
         return loss
     
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
         
-        if self.hparams.loss_function == "mse":
-            loss = F.mse_loss(y_hat, y, reduction="mean")
-        elif self.hparams.loss_function == "poisson":
-            if self.hparams.log_input:
-                loss = F.poisson_nll_loss(y_hat, x, log_input=True, reduction="mean")
-            else:
-                loss = F.poisson_nll_loss(y_hat, x, log_input=False, reduction="mean")
+        loss = F.mse_loss(y_hat, y, reduction="mean")
 
         result = torch.stack([y_hat.cpu().detach(), y.cpu().detach()], dim=1)
 
-        self.log(str(self.hparams.loss_function) + " val_loss", loss)
+        self.log("val_loss", loss)
         return result
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
         
-        if self.hparams.loss_function == "mse":
-            loss = F.mse_loss(y_hat, y, reduction="mean")
-        elif self.hparams.loss_function == "poisson":
-            if self.hparams.log_input:
-                loss = F.poisson_nll_loss(y_hat, x, log_input=True, reduction="mean")
-            else:
-                loss = F.poisson_nll_loss(y_hat, x, log_input=False, reduction="mean")
+        loss = F.mse_loss(y_hat, y, reduction="mean")
     
-        self.log(str(self.hparams.loss_function) + " test_loss", loss)
+        self.log("test_loss", loss)
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         x, y = batch
@@ -144,43 +204,38 @@ class Base_2(pl.LightningModule):
         return torch.cat((y_hat.cpu().detach(), y.cpu().detach()), dim=1) # batch_size * (2 * neuron_num)
 
     
-# simulated_network_type:
-#   1: W_ij @ F.tanh(X_t) + b
-#   2: F.tanh(W_ij @ X_t + b)
-#   3. W_ij @ X_t + b
-class Baseline_2(Base_2):
+#   tanh(W_ij @ X_t + b)
+#   exp(W_ij @ X_t + b)
+class GLM_sim(Base_2):
     def __init__(
         self,
         neuron_num=10,
         learning_rate=1e-4,
-        simulated_network_type=3,
         model_random_seed=42,
         scheduler="cycle",
-        loss_function="mse",   # mse, poisson
-        log_input=False,
+        loss_function="mse",   # mse
         weight_decay=0,
+        activation_type="tanh",   # tanh, exp
     ):
         super().__init__()
         self.save_hyperparameters()
 
         torch.manual_seed(model_random_seed)
 
-        self.simulated_network_type = simulated_network_type
-
-        self.activation = nn.Tanh()
+        self.activation_type = activation_type
+        if activation_type == "tanh":
+            self.activation = nn.Tanh()
 
         self.W = nn.Linear(neuron_num, neuron_num, bias=False)
         self.b = nn.Parameter(torch.zeros(neuron_num))
        
     def forward(self, x): # x: batch_size * (neuron_num)
-        if self.simulated_network_type == 1:
+        x = self.W(x) + self.b
+
+        if self.activation_type == "tanh":
             x = self.activation(x)
-            x = self.W(x) + self.b
-        elif self.simulated_network_type == 2:
-            x = self.W(x) + self.b
-            x = self.activation(x)
-        elif self.simulated_network_type == 3:
-            x = self.W(x) + self.b
+        elif self.activation_type == "exp":
+            x = torch.exp(x)
         return x
     
 
@@ -192,7 +247,7 @@ class Baseline_2(Base_2):
 # - x_{t+1} = \sum A_k x_{t-k}, where k is the number of tau(s)
 ###################################################################################################
 
-class Base_2(pl.LightningModule):
+class Base_3(pl.LightningModule):
     def __init__(self,) -> None:
         super().__init__()
         self.save_hyperparameters()
@@ -259,7 +314,7 @@ class Base_2(pl.LightningModule):
     
 
 # x_{t+1} = \sum A_k x_{t-k}, where k is the number of tau(s)
-class GLM_M(Base_2):
+class GLM_M(Base_3):
     def __init__(
         self,
         num_neurons,
